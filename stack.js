ec75a9844cdbe1f575ee43ab908f1dd35c22b001
Hassan Kazi <andaijja@gmail.com>
2023-01-05 14:58:12 -0500

    finished stack

--- stack.js
+++ stack.js
@@ -21,25 +21,52 @@ class Stack {
 
   push(val) {
 
+    const newNode = new Node(val);
+
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      newNode.next = this.first;
+      this.first = newNode;
+    }
+    this.size++;
+    return undefined;
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
+    if (this.size === 0) {
+      throw new Error("Empty Stack");
+    }
 
+    let removed = this.first;
+    this.first = this.first.next;
+
+    this.size--;
+    return removed.val;
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
 
+    if (this.first) {
+      return this.first.val;
+    }
+    return undefined;
+
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    if (this.size === 0) {
+      return true;
+    }
+    return false;
   }
 }
 
